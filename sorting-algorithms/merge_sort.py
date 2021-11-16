e30dd3606d65fce7194a1aa05b84ebfe467c0841
Ivan Arena <ivan.x28x@gmail.com>
2021-11-16 18:29:55 +0100

    Put merging process in a separeted function

--- sorting-algorithms/merge_sort.py
+++ sorting-algorithms/merge_sort.py
@@ -1,37 +1,40 @@
 numbers = [9, 4, 5, 6, 1, 7, 2, 5]
 
 
-def merge_sort(numbers):
-    if len(numbers) > 1:
-        mid = len(numbers) // 2
-        left = numbers[:mid]
-        right = numbers[mid:]
+def merge(items, left, right):
+    l = r = 0
+    k = 0
+
+    while l < len(left) and r < len(right):
+        if left[l] <= right[r]:
+            items[k] = left[l]
+            l += 1
+        else:
+            items[k] = right[r]
+            r += 1
+        k += 1
+
+    while l < len(left):
+        items[k] = left[l]
+        l += 1
+        k += 1
+
+    while r < len(right):
+        items[k] = right[r]
+        r += 1
+        k += 1
+
+
+def merge_sort(items):
+    if len(items) > 1:
+        mid = len(items) // 2
+        left = items[:mid]
+        right = items[mid:]
         merge_sort(left)
         merge_sort(right)
-
-        l = r = 0
-        k = 0
-
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                numbers[k] = left[l]
-                l += 1
-            else:
-                numbers[k] = right[r]
-                r += 1
-            k += 1
-
-        while l < len(left):
-            numbers[k] = left[l]
-            l += 1
-            k += 1
-
-        while r < len(right):
-            numbers[k] = right[r]
-            r += 1
-            k += 1
+        merge(items, left, right)
 
 
-print('numbers', numbers)
+print('numbers:', numbers)
 merge_sort(numbers)
 print('sorted:', numbers)
