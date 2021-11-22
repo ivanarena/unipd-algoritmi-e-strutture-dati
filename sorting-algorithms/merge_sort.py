7c9c54636563ce3558a47298257ae287860634d1
Ivan Arena <ivan.x28x@gmail.com>
2021-11-22 13:49:58 +0100

    Updated version
    Now left and right arrays are managed in the merge function as the professor's pseudo-code does.

--- sorting-algorithms/merge_sort.py
+++ sorting-algorithms/merge_sort.py
@@ -1,9 +1,12 @@
 numbers = [9, 4, 5, 6, 1, 7, 2, 5]
 
 
-def merge(items, left, right):
-    l = r = 0
-    k = 0
+def merge(items, start, mid, end):
+    left = items[start:mid+1]
+    right = items[mid+1:end+1]
+
+    l = r = 0  # left and right iterators
+    k = start  # main list iterator
 
     while l < len(left) and r < len(right):
         if left[l] <= right[r]:
@@ -14,27 +17,25 @@ def merge(items, left, right):
             r += 1
         k += 1
 
-    while l < len(left):
+    while l < len(left):  # add remaining items
         items[k] = left[l]
         l += 1
         k += 1
 
-    while r < len(right):
+    while r < len(right):  # add remaining items
         items[k] = right[r]
         r += 1
         k += 1
 
 
-def merge_sort(items):
-    if len(items) > 1:
-        mid = len(items) // 2
-        left = items[:mid]
-        right = items[mid:]
-        merge_sort(left)
-        merge_sort(right)
-        merge(items, left, right)
+def merge_sort(items, start, end):
+    if start < end:
+        mid = (start + end) // 2
+        merge_sort(items, start, mid)
+        merge_sort(items, mid + 1, end)
+        merge(items, start, mid, end)
 
 
 print('numbers:', numbers)
-merge_sort(numbers)
+merge_sort(numbers, 0, len(numbers) - 1)
 print('sorted:', numbers)
